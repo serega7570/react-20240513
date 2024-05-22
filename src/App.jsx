05685d52298cfa430891939f9ade39387f7f6d78
Сергей <s.plykin@rt-ib.ru>
2024-05-22 17:02:09 +0300

    Домашнее задание 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
+import { useState } from 'react';
 import Restaurant from './components/restaurant';
 import { restaurants } from './consts/mock';
+import restaurant from './components/restaurant';
 
 const App = () => {
+    const [activeId, setActiveId] = useState(restaurants[0].id);
+
+    const activeRestaurant = restaurants.find((restaurant) => restaurant.id === activeId);
+
     return (
         <>
-            {restaurants.map((restaurant) => {
-                return <Restaurant restaurant={restaurant} />;
-            })}
+            <ul class="nav">
+                {restaurants.map((restaurant) => {
+                    return (
+                        <li class="nav-item">
+                            <h4>
+                                <a class="nav-link ps-0 pe-5 cursor-pointer" onClick={() => setActiveId(restaurant.id)}>
+                                    {restaurant.name}
+                                </a>
+                            </h4>
+                        </li>
+                    );
+                })}
+            </ul>
+            <Restaurant restaurant={activeRestaurant} />
         </>
     );
 };
--- src/components/dish/dish.jsx
+++ src/components/dish/dish.jsx
@@ -1,5 +1,18 @@
+import { useState } from 'react';
+
 const Dish = ({ dish }) => {
-    return <li>{dish.name}</li>;
+    const [count, setCount] = useState(0);
+
+    const handleChangeCount = (e) => {
+        if (e.target.value < 0 || e.target.value > 5) return;
+        setCount(e.target.value);
+    };
+    return (
+        <li>
+            {dish.name}
+            <input className="ms-1 w-25" min={0} max={5} value={count} type="number" onChange={handleChangeCount} />
+        </li>
+    );
 };
 
 export default Dish;
